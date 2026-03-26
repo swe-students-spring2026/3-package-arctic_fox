672686400a29fe6f4adcf3c3186451903be4c49c
Sanjayc0204 <sanjaychunduru@gmail.com>
2026-03-26 14:19:36 -0400

    add function skeletons

---
+++ src/placeholderproject/movies.py
@@ -0,0 +1,22 @@
+"""
+Create your movie methods here
+"""
+
+def movie_night_picker():
+    # params: genres_to_avoid=None, runtime_max=150, minimum_rating
+    pass
+
+
+def quiz():
+    # params: attributes=["Director", "Runtime", "Year", etc]
+    pass
+
+
+def lead_actor():
+    # params: actor= 
+    pass
+
+
+def find_similar():
+    # params: movie_name=, attributes=["Director", "Runtime", etc]
+    pass
